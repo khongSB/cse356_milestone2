f63df0a9ed66a8672aa80e2e69898b1d8e31c8ae
root <root@bubbleguppies.cse356.compas.cs.stonybrook.edu>
2024-11-28 19:11:07 +0000

    Added HAProxies

--- app/backend/server.js
+++ app/backend/server.js
@@ -15,6 +15,43 @@ const mediaRouter = require("./routes/mediaRouter.js");
 mongoose.connect(mongoURI);
 const db = mongoose.connection;
 
+const { exec } = require('child_process');
+const fs = require("fs");
+
+const uploaded_video_table = require("./models/uploaded-video");
+
+// Read the JSON file
+fs.readFile("../media/m2.json", "utf8", (err, data) => {
+  if (err) {
+    console.error("Error reading the file:", err);
+    return;
+  }
+
+  // Parse the JSON data
+  const videos = JSON.parse(data);
+
+  // Process each entry in the JSON file
+  Object.keys(videos).forEach(async (key) => {
+    const video = {
+      user_id: "bob", // Static user_id
+      video_id: key.replace(".mp4", ""), // Remove .mp4 suffix
+      title: key.replace(".mp4", ""), // Same as video_id
+      author: "bob", // Static author
+      description: videos[key], // Description is the value in the JSON
+      status: "complete", // Set status as 'complete'
+    };
+
+    // Create and save a new video entry
+    try {
+      const newVideo = new uploaded_video_table(video);
+      await newVideo.save();
+      console.log(`Inserted: ${video.title}`);
+    } catch (error) {
+      console.error(`Error inserting ${video.title}:`, error);
+    }
+  });
+})
+
 // const rateLimit = require("express-rate-limit");
 // const limiter = rateLimit({
 //   windowMs: 1 * 60 * 1000, // 1 minute
@@ -74,6 +111,10 @@ app.use((req, res, next) => {
   console.log("--- End of Request Information ---\n");
 
   next(); // Call next middleware
+});
+
+app.get('/health', (req, res) => {
+  res.status(200).send('OK'); // Return a 200 status code with a message
 });
 
 // Add user functionality
