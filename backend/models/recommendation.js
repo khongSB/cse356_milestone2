8b522df323058a2940bd58f6e77c94cb960aec96
root <root@bubbleguppies.cse356.compas.cs.stonybrook.edu>
2024-11-28 06:50:34 +0000

    Deleting unused folders from milestone2 commit

--- backend/models/recommendation.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const { Schema } = mongoose;
-
-const RecommendationSchema = new Schema({
-  user_id: { type: String, required: true },             // ID of the user receiving recommendations
-  recommended_videos: [{ type: String, required: true }] // Array of recommended video IDs
-});
-
-module.exports = mongoose.model("Recommendation", RecommendationSchema);
